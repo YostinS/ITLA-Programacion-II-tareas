92b79a99497514514cedf6ab0ef00fa64e1878fc
YostinS <yostinsanchezrosario@gmail.com>
2025-05-24 23:18:08 -0400

    Tarea 1 Mapa de Clases

---
+++ Prog2_MapaClases_YostinS/Prog2_MapaClases_YostinS/Program.cs
@@ -0,0 +1,62 @@
+﻿public class MiembroDeLaComunidad
+{
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+    public int Genero { get; set; }
+
+    public void MostrarInfo()
+    {
+        Console.WriteLine($"Nombre: {Nombre}, Edad: {Edad}, Genero: {Genero}");
+    }
+}
+public class Empleado : MiembroDeLaComunidad
+{
+    public string Cargo { get; set; }
+
+    public void MostrarInfo()
+    {
+        base.MostrarInfo();
+        Console.WriteLine($"Cargo: {Cargo}");
+    }
+}
+public class Estudiante : MiembroDeLaComunidad
+{
+    public string Carrera { get; set; }
+    public string Matricula { get; set; }
+
+    public void MostrarInfo()
+    {
+        base.MostrarInfo();
+        Console.WriteLine($"Carrera: {Carrera}, Matricula: {Matricula}");
+    }
+}
+public class ExAlumno : MiembroDeLaComunidad
+{
+    public int FechaEgreso { get; set; }
+
+    public  void MostrarInfo()
+    {
+        base.MostrarInfo();
+        Console.WriteLine($"Fecha de Egreso: {FechaEgreso}");
+    }
+}
+public class Docente : Empleado
+{
+    public string Materia { get; set; }
+
+    public void MostrarInfo()
+    {
+        base.MostrarInfo();
+        Console.WriteLine($"Materia a impartir: {Materia}");
+    }
+}
+public class Administrador : Docente
+{
+    public string NivelAcceso { get; set; }
+
+    public void MostrarInfo()
+    {
+        base.MostrarInfo();
+        Console.WriteLine($"Nivel de Acceso: {NivelAcceso}");
+    }
+}
