f5cf416404ce7d6e7770502639c6d5023490ceca
YostinS <yostinsanchezrosario@gmail.com>
2025-07-28 23:49:21 -0400

    Tarea 3/4 actualizacion

--- CentroCapacitacionOficios/CentroCapacitaciones.Infrastructure/obj/Debug/net8.0/CentroCapacitacionOficios.Infrastructure.AssemblyInfo.cs
+++ CentroCapacitacionOficios/CentroCapacitaciones.Infrastructure/obj/Debug/net8.0/CentroCapacitacionOficios.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CentroCapacitacionOficios.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a659e1de97bad44517bc5dfd56f637614f49918c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e63051ab97bc4ce99d3915288053d59928f532d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CentroCapacitacionOficios.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CentroCapacitacionOficios.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
